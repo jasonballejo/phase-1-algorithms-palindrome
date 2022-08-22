76ca47367b15180add8422622c1118bfb88c0b70
Jason Ballejo <ballejo19@gmail.com>
2022-08-22 14:56:13 -0400

    commit tree 0845ad7c1826c212a8efcd606e4ea45fd66eb3ca

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  let reversed = word.split('').reverse().join('')
+  console.log(reversed)
+    if(reversed === word) return true
+
+    return false
+  }
+/*
+  from writing a function called isPalindrome, return true if the word given is a palindrom. If not, then it needs to return false.
+*/
 
 /* 
   Add your pseudocode here
+  write a function if the word is a palindrom then return true else return false
 */
 
 /*
   Add written explanation of your solution here
+  We start by creating a variable to first grab the word give and we split the word and revese it
 */
 
 // You can run `node index.js` to view these console logs
